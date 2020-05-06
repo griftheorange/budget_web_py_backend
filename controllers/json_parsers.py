46df412a7d201cf6a439019c02e13de868307cba
Griffin Poole <griffinpoole5@gmail.com>
2020-05-06 18:17:38 -0400

    Revert "Simplified API route for data fetch"
    This reverts commit 1686a9c04deab26f81a0c99d4243f4de3aa19be7.

--- controllers/json_parsers.py
+++ controllers/json_parsers.py
@@ -9,7 +9,7 @@ class JSONParsers:
     # MAIN SERIALIZER FOR APP
     # Gets full table data and formatted line graph data from data handler
     # Packages both sets into relevant keys, return json object
-    def fetch_data():
+    def fetch_data(filename):
         # Some columns need formatting to comma-separated 2-decimal dollar values
         format_set = {}
         table_data = DH.get_data().to_dict()
